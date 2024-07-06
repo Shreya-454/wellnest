277e44ebead0fbbd527eca4eebbf03cec4fc210c
Shreya-454 <bshreya932@GMAIL.COM>
2024-07-06 16:14:49 +0530

    button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { CommonPara } from './common/Headings'
+import { Button } from './common/Button'
 const Hero = () => {
   return (
     <div className='min-h-[810px] bg-[url(./assets/images/webp/headerImages/heroBgImg.webp)] bg-cover bg-center bg-no-repeat bg-lightOrange'>
       <div className="container mx-auto px-3 max-w-[1164px] pt-20">
-        <h1 className='text-custom_6xl font-poppins font-bold text-black text-center leading-120'>Welcome To <span className='text-purple leading-120'>Wellnest</span></h1>
-      </div>
+        <h1 className='text-custom_6xl font-poppins font-bold text-black text-center leading-120 mb-4'>Welcome To <span className='text-purple leading-120'>Wellnest</span></h1>
+        <CommonPara text="Supporting Individuals And Companies In Achieving Their Highest Potential For The Betterment Of Themselves And Our Community." className="text-center max-w-[642px] mx-auto mb-10"/>
+<div className='flex justify-center'><Button btnName="Book A Free Consultation"/></div>
+      </div> 
     </div>
   )
 }
