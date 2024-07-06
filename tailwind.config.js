1a949a6b51c86c20dbff7e6bb69bba1bf7182fd9
Shreya-454 <bshreya932@GMAIL.COM>
2024-07-06 15:20:30 +0530

    repo setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'poppins': ['Poppins','sans-serif'],
+        'inter':['Inter','sans-serif']
+      },
+      fontSize:{
+        'custom_lg':'19px',
+        'custom_5xl':'50px',
+        'custom_6xl':'64px',
+      },
+      lineHeight:{
+        120:"120%",
+        150:"150%",
+        180:"180%",
+    },
+    colors:{
+      purple:"#A18EE8",
+      lightBlack:"#1E2031",
+      lightGreen:"#C5FCC3",
+      skyBlue:"#AEEFF4",
+      darkGreen:"#4CDFB2",
+      orange:"#FFB685",
+      offBlack:"#OEDF13",
+      darkSkyBlue:"#DA66C2",
+    },
   },
+},
   plugins: [],
-}
-
+};
