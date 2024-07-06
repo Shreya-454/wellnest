1d5fc558d9ecb5acecb18e9f14b6d37b831c4326
Shreya-454 <bshreya932@GMAIL.COM>
2024-07-06 21:54:14 +0530

    header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='min-h-[810px] bg-[url(./assets/images/webp/headerImages/heroBgImg.webp)] bg-cover bg-center bg-no-repeat bg-lightOrange'>
       <div className="container mx-auto px-4 2xl:max-w-[1320px] max-w-[1164px] pt-20">
-        <h1 className='text-custom_6xl font-poppins font-bold text-black text-center leading-120 mb-4'>Welcome To <span className='text-purple leading-120'>Wellnest</span></h1>
+        <h1 className='md:text-custom_6xl sm:text-5xl text-4xl font-poppins font-bold text-black text-center leading-120 mb-4'>Welcome To <span className='text-purple leading-120'>Wellnest</span></h1>
         <CommonPara text="Supporting Individuals And Companies In Achieving Their Highest Potential For The Betterment Of Themselves And Our Community." className="text-center max-w-[642px] mx-auto mb-10"/>
 <div className='flex justify-center mb-12'><Button btnName="Book A Free Consultation"/></div>
 <div className="flex flex-row flex-wrap -mx-3">
