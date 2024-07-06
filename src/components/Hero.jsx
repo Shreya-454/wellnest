bf075b189ee91bd1c96fc55607ef69e7d731e52b
Shreya-454 <bshreya932@GMAIL.COM>
2024-07-06 16:01:14 +0530

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-[url(./assets/images/webp/headerImages/heroBgImg.webp)] bg-cover bg-center bg-no-repeat bg-lightOrange'>
+      <div className="container mx-auto px-3 max-w-[1164px] pt-20">
+        <h1 className='text-custom_6xl font-poppins font-bold text-black text-center'>Welcome To <span className='text-purple'>Wellnest</span></h1>
+      </div>
     </div>
   )
 }
