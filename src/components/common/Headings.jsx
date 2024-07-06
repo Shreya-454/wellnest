efdf248f926f99ee7b2acc94cc5097f42c832ced
Priyanka-390 <psr82220@gmail.com>
2024-07-06 15:36:59 +0530

    common heading

--- src/components/common/Headings.jsx
+++ src/components/common/Headings.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
-export const Headings = () => {
+export const CommonHeading = ({
+  purpleHeading,heading
+}) => {
   return (
-    <div>Headings</div>
+    <h1
+      className={`font-poppins text-black font-semibold leading-120 text-5xl ${className}`}
+    >{heading}
+      <span className="text-purple">{purpleHeading}</span>
+    </h1>
+  );
+};
+
+export const CommonPara = (text) => {
+  return (
+    <p className={`font-poppins text-base font-normal leading-150 text-black opacity-70 ${className}`}>{text}</p>
   )
 }
+
+
