60f53b583b2b140d1175ba5f3efbd0b347fe763c
Shreya-454 <bshreya932@GMAIL.COM>
2024-07-10 20:55:26 +0530

    faq ellipse size

--- src/components/Meet.jsx
+++ src/components/Meet.jsx
@@ -8,7 +8,7 @@ const Meet = () => {
     <div className=' relative' id='about'>
         <img src={meetEllipse} alt="ellipse" className='absolute top-0  object-cover w-full z-[-1]' />
       <div className='max-w-[1440px] mx-auto xl:pt-[405px] lg:pt-56 sm:pt-32 pt-24'>
-        <div className="container max-w-[1322px] px-4 xl:-translate-x-[4%]">
+        <div className="container max-w-[1322px] px-4 xl:-translate-x-[5%]">
       <div className="flex flex-row flex-wrap -mx-3">
       <div className="lg:w-1/2 w-full flex justify-center px-3">
       <div className='h-auto max-lg:mb-7 max-sm:mb-5'>
